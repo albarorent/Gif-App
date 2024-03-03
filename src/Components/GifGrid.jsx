2e65936e9fc08f67f05891e9d6314cd67b25970d
Alvaro <alvarorent2001@gmail.com>
2024-03-03 04:40:04 -0500

    Update

--- src/Components/GifGrid.jsx
+++ src/Components/GifGrid.jsx
@@ -17,6 +17,9 @@ export default function GifGrid({ category }) {
   return (
     <div className="flex flex-col gap-5 pt-5">
       <h3 className="text-4xl font-semibold">{category}</h3>
+      <button className="text-center bg-red-600 p-1 w-28 rounded-lg text-slate-100">
+        Borrar
+      </button>
       <div className="card-grid">
         {gif.map((gifs) => (
           <GifItem key={gifs.id} {...gifs} />
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -11,10 +11,14 @@ export default function Home() {
     setCategories([newCategory, ...categories]);
   };
 
+  const onDeleteCategory = () =>{
+    
+  }
+
   return (
     <>
       <div className="grid justify-center gap-8">
-        <h1 className="text-3xl font-bold text-green-600">
+        <h1 className="text-3xl font-bold text-emerald-600">
           Todos los GIFS del mundo en un solo lugar.
         </h1>
         <p className="text-center font-semibold">Busca, mira y diviertete</p>
