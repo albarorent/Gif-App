235a20030ffb77cb49ca324dee7ac4d3c8e2ac0e
Alvaro <alvarorent2001@gmail.com>
2024-03-03 22:44:48 -0500

    update

--- src/helpers/getGifs.ts
+++ src/helpers/getGifs.ts
@@ -1,11 +1,21 @@
-export const gefGifs = async (category) => {
+export const gefGifs = async (category:string) => {
   const url = `https://api.giphy.com/v1/gifs/search?api_key=ASmuPBF0o2gT0vge3J2oBvJeyzpMPWpt&q=${category}&limit=6`;
   const resp = await fetch(url);
   const { data } = await resp.json();
-  const gifs = data.map((img) => ({
+  const gifs = data.map((img:ImageData) => ({
     id: img.id,
     title: img.title,
     url: img.images.downsized_medium.url,
   }));
   return gifs;
 };
+
+interface ImageData {
+  id: number;
+  title: string;
+  images: {
+    downsized_medium: {
+      url: string;
+    };
+  };
+}
